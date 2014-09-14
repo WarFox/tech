faf124b7eee141d8d2ba358647f43a8942f4e788
Deepu Mohan Puthrote <github@deepumohan.com>
2014-09-14 10:49:00 +0530

    Added bootstrapjs as dependency

--- .themes/warfox/source/javascripts/main.js
+++ .themes/warfox/source/javascripts/main.js
@@ -12,17 +12,22 @@
         "github": {
           deps: ['jquery'],
           exports: 'github'
+        },
+        "bootstrap" : {
+          deps: ['jquery'],
+          exports: 'bootstrap'
         }
       },
       "paths": {
         "modernizer": "//cdn.jsdelivr.net/modernizr/2.8.3/modernizr.min",
         "jquery": "//cdn.jsdelivr.net/jquery/1.9.1/jquery-1.9.1.min",
+        "bootstrap": '//cdn.jsdelivr.net/bootstrap/3.2.0/js/bootstrap.min',
         "octopress": "octopress",
         "github": "github"
       }
   });
 
-  requirejs(["jquery", 'app', "modernizer", 'octopress'], function ($, app) {
+  requirejs(["jquery", 'app', "modernizer", 'octopress', 'bootstrap'], function ($, app) {
     app.github();
   });
 
