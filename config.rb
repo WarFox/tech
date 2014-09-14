23bf157ec67f53dd87444c1cf54d96026c3215eb
Deepu Mohan Puthrote <github@deepumohan.com>
2014-09-14 06:16:30 +0530

    Updated paths in config.rb

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/tech"
+http_images_path = "/tech/images"
+http_generated_images_path = "/tech/images"
+http_fonts_path = "/tech/fonts"
+css_dir = "public/tech/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
